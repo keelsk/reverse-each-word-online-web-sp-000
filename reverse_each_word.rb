0fcb09ddb2bc106f6c383173f4e9672d2fb4440a
Kirsten Keels <keelsk@gmail.com>
2020-02-23 18:38:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 
 def reverse_each_word(sentence)
   sentence = sentence.split(" ")
-  new_array = []
-  sentence.collect do |word|
+  reversed_words = sentence.collect do |word|
     new_array << word.reverse
   end
-  new_array.join(" ")
+  reversed_words.join(" ")
 end
 
