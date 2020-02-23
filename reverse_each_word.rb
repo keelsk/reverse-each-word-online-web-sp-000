8760ad0019a3194e04581d3680452554180723ac
Kirsten Keels <keelsk@gmail.com>
2020-02-23 18:38:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(sentence)
   sentence = sentence.split(" ")
   reversed_words = sentence.collect do |word|
-    new_array << word.reverse
+    word.reverse
   end
   reversed_words.join(" ")
 end
