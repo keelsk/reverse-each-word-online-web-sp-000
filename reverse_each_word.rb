cbbd43a136d89f562a4707ac0c0c4a29ef7572c3
Kirsten Keels <keelsk@gmail.com>
2020-02-23 17:52:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   sentence_array.collect do |word|
-    word.reverse
+    "#{word.reverse}"
   end
   binding.pry
 end
